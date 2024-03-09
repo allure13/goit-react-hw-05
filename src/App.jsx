f96e52479b01f167c9f80f4a7c97c75c01e108b8
Ihor Chepliaka <ihor.chepliaka@gmail.com>
2024-03-09 15:43:17 +0200

    start
    почав робити маршрути в апп

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-import { Heading } from 'components';
+import { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const HomePage = lazy(() => import('pages/HomePage'));
+const MoviesPage = lazy(() => import('pages/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('pages/MovieDetailsPage'));
+const MovieCast = lazy(() => import('components/MovieCast/MovieCast'));
+const MovieReviews = lazy(() => import('components/MovieReviews/MovieReviews'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 
 export const App = () => {
-  return <Heading title="App" bottom />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<MovieCast />} />
+          <Route path="reviews" element={<MovieReviews />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
+  );
 };
