682e7561c3d06e7aef05cc54c0e0d8dcdf518310
Ihor Chepliaka <ihor.chepliaka@gmail.com>
2024-03-11 18:07:16 +0200

    add css for nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import css from './Navigation.module.css';
 import clsx from 'clsx';
 
 const makeLinkClass = ({ isActive }) => {
-  return clsx(css.link, isActive && css.Active);
+  return clsx(css.link, isActive && css.active);
 };
 
 export default function Navigation() {
